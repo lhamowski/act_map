1a800aa8865ad2dd6bc6cdbb0e18823e56e4037f
Lukasz Hamowski <lukaszhamowski@gmail.com>
2019-11-06 18:59:45 +0100

    Add rest of iterators, find, at, operator[], count

--- include/act/map.hpp
+++ include/act/map.hpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <array>
 #include <utility>
+#include <optional>
 
 namespace act::details
 {
@@ -27,6 +28,11 @@ public:
     using value_type = std::pair<key_type, mapped_type>;
     using container_type = std::array<value_type, size>;
     using size_type = typename container_type::size_type;
+    using iterator = typename container_type::iterator;
+    using const_iterator = typename container_type::const_iterator;
+    using reverse_iterator = typename container_type::reverse_iterator;
+    using const_reverse_iterator =
+      typename container_type::const_reverse_iterator;
 
     template<typename... Args>
     constexpr map(Args&&... elements) :
@@ -34,7 +40,52 @@ public:
     {
     }
 
+    constexpr std::optional<value_type> find(const key_type& key) const
+    {
+        auto actual = cbegin();
+        auto last = cend();
+
+        for (; actual != last; ++actual)
+        {
+            if (actual->first == key)
+            {
+                return {*actual};
+            }
+        }
+        
+        return std::nullopt;
+    }
+
+    constexpr std::optional<mapped_type> at(const key_type& key) const
+    {
+        auto result = find(key);
+
+        if (result != std::nullopt)
+            return result->second;
+
+        return std::nullopt;
+    }
+
+    constexpr auto operator[](const key_type& key) const
+    {
+        return at(key);
+    }
+
+    constexpr size_type count(const key_type& key) const
+    {
+        return find(key) != std::nullopt ? 1 : 0;
+    }
+
     constexpr size_type size() const { return elements_.size(); }
+
+    constexpr auto begin()         { return elements_.begin();   }
+    constexpr auto end()           { return elements_.end();     }
+    constexpr auto rbegin()        { return elements_.rbegin();  }
+    constexpr auto rend()          { return elements_.rend();    }
+    constexpr auto cbegin()  const { return elements_.cbegin();  }
+    constexpr auto cend()    const { return elements_.cend();    }
+    constexpr auto crbegin() const { return elements_.crbegin(); }
+    constexpr auto crend()   const { return elements_.crend();   }
 
 private:
     container_type elements_;
